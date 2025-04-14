877c55527e0317d29c2b0fb0a012c3e3248532ca
Fabio Gentili <fabio1.gentili@consulenti.fastweb.it>
2025-04-14 17:54:43 +0200

    Creazione della funzione di inserimento in tabella

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import requests
 import os
 import configparser
-
+import csv
+import psycopg2
 
 def download_file(url=None):
     # Leggi il file di configurazione
@@ -42,3 +43,57 @@ def download_file(url=None):
     except IOError as e:
         print(f"Errore durante il salvataggio del file: {e}")
 
+def load_csv_to_postgresql():
+    config = configparser.ConfigParser()
+    # Assicurati che il file config.ini esista nella stessa directory dello script
+    config.read('config.ini')
+
+    # Ottieni la directory di staging e l'URL dal file di configurazione
+    csv_directory = config.get('Directories', 'dir-staging')
+    csv_filename = config.get('Directories', 'staging-file')
+    table_name = config.get('DB', 'stage-table')
+
+     # Parametri di connessione al database
+    db_params = {
+        'host': config.get('DB','host'),
+        'database': config.get('DB','database'),
+        'user': config.get('DB', 'user'),
+        'password': config.get('DB', 'password')
+    }
+
+    # Percorso completo del file CSV
+    csv_path = os.path.join(csv_directory, csv_filename)
+
+    try:
+        # Connessione al database
+        conn = psycopg2.connect(**db_params)
+        cur = conn.cursor()
+
+        # Esegui TRUNCATE sulla tabella
+        cur.execute(f"TRUNCATE TABLE {table_name}")
+
+        # Apri e leggi il file CSV
+        with open(csv_path, 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            headers = next(csvreader)  # Leggi l'intestazione
+
+            # Prepara la query di inserimento
+            insert_query = f"INSERT INTO {table_name} ({','.join(headers)}) VALUES ({','.join(['%s']*len(headers))})"
+
+            # Inserisci i dati riga per riga
+            for row in csvreader:
+                cur.execute(insert_query, row)
+
+        # Commit delle modifiche
+        conn.commit()
+        print(f"Dati caricati con successo nella tabella {table_name}")
+
+    except (Exception, psycopg2.Error) as error:
+        print(f"Errore durante l'esecuzione dello script: {error}")
+
+    finally:
+        # Chiudi la connessione al database
+        if conn:
+            cur.close()
+            conn.close()
+            print("Connessione al database chiusa.")
